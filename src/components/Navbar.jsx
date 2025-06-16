1b08de61aaa83aae8a637cfb272fbf3b22e1ad88
Masudcodeclouds <masud.molla@codeclouds.co.in>
2025-06-16 11:59:42 +0530

    Changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,40 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav mx-auto">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+       
+        
+      </ul>
+      <ul class="navbar-nav mx-right">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Signup</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+       
+        
       </ul>
     </div>
   </div>
 </nav>
+    </div>
+  );
+};
+
+export default Navbar;
